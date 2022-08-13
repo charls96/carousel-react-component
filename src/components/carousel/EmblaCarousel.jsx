bc738e9cffcd35e23bf173a41a23bce2e10ffef7
charls96 <charly.lopez.perez@gmail.com>
2022-08-13 13:32:50 +0200

    added dot buttons

--- src/components/carousel/EmblaCarousel.jsx
+++ src/components/carousel/EmblaCarousel.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
-import { PrevButton, NextButton } from "./EmblaCarouselButtons";
+import { DotButton, PrevButton, NextButton } from "./EmblaCarouselButtons";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 import "./styles/embla.css";
@@ -16,6 +16,10 @@ const EmblaCarousel = ({ slides, options = { loop: false, delay: 5000 } }) => {
   const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
   const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
 
+  //dots
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState([]);
+
   const scrollNext = useCallback(() => {
     if (!emblaApi) return;
     emblaApi.scrollNext();
@@ -28,38 +32,60 @@ const EmblaCarousel = ({ slides, options = { loop: false, delay: 5000 } }) => {
     autoplay.current.reset();
   }, [emblaApi]);
 
+  //dots
+  const scrollTo = useCallback(
+    (index) => {
+        emblaApi.scrollTo(index);
+        autoplay.current.reset();
+    },
+    [emblaApi]
+  );
+
   const onSelect = useCallback(() => {
     if (!emblaApi) return;
+    setSelectedIndex(emblaApi.selectedScrollSnap());
     setPrevBtnEnabled(emblaApi.canScrollPrev());
     setNextBtnEnabled(emblaApi.canScrollNext());
-  }, [emblaApi]);
+  }, [emblaApi, setSelectedIndex]);
 
   useEffect(() => {
     if (!emblaApi) return;
     onSelect();
+    setScrollSnaps(emblaApi.scrollSnapList());
     emblaApi.on("select", onSelect);
-  }, [emblaApi, onSelect]);
+  }, [emblaApi, setScrollSnaps, onSelect]);
 
   return (
-    <div className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {slides.map((image, index) => (
-            <div className="embla__slide" key={index}>
-              <div className="embla__slide__inner">
-                <img
-                  className="embla__slide__img"
-                  src={image}
-                  alt="A cool cat."
-                />
+    <>
+      <div className="embla">
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {slides.map((image, index) => (
+              <div className="embla__slide" key={index}>
+                <div className="embla__slide__inner">
+                  <img
+                    className="embla__slide__img"
+                    src={image}
+                    alt="A cool cat."
+                  />
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
+        <PrevButton onClick={scrollPrev} enabled={prevBtnEnabled} />
+        <NextButton onClick={scrollNext} enabled={nextBtnEnabled} />
       </div>
-      <PrevButton onClick={scrollPrev} enabled={prevBtnEnabled} />
-      <NextButton onClick={scrollNext} enabled={nextBtnEnabled} />
-    </div>
+      <div className="embla__dots">
+        {scrollSnaps.map((_, index) => (
+          <DotButton
+            key={index}
+            selected={index === selectedIndex}
+            onClick={() => scrollTo(index)}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 

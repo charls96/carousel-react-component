4c0b0d2fc744ef453926c223312f5ef02b70b452
charls96 <charly.lopez.perez@gmail.com>
2022-08-13 14:11:34 +0200

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import EmblaCarousel from "./components/carousel/EmblaCarousel"
-import tiger from "./assets/carousel/tiger.jpg"
-import tigerLite from "./assets/carousel/tiger_lite.jpg"
-import twist from "./assets/carousel/twist.webp"
+import tiger from "./assets/carousel/tiger.webp"
+import d60Lite from "./assets/carousel/d60_lite.webp"
+import maja from "./assets/carousel/maja.webp"
 
 function App() {
-  const slides = [tiger, tigerLite, twist];
+  const slides = [tiger, d60Lite, maja];
 
   return (
     <div className="App">
+      <h1>Full size carousel</h1>
       <EmblaCarousel slides={slides}/>
     </div>
   )
